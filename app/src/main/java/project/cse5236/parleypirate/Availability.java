b3332d6069df1a1034ee68981ece808cd474ecb8
lee7226 <bxlee1441@gmail.com>
2018-11-01 13:15:01 -0400

    Added GetOpenAvailability
    small addition of a function for finding open availabilities

--- app/src/main/java/project/cse5236/parleypirate/Availability.java
+++ app/src/main/java/project/cse5236/parleypirate/Availability.java
@@ -3,4 +3,25 @@ package project.cse5236.parleypirate;
 public class Availability {
     private String userId;
     private String availability;
+
+    public String GetOpenAvalability(String[] avals) {
+        String openAval = "";
+        int[] intRepresentations = new int[avals.length];
+
+        // convert all given avals to int then to binary
+        for (int i = 0; i < avals.length; i++) {
+            intRepresentations[i] = Integer.parseInt(avals[i], 2);
+        }
+
+        int bitwisedAvals = 0;
+
+        // do bitwise or on binary avals
+        for (int i = 0; i < intRepresentations.length; i++) {
+            bitwisedAvals = bitwisedAvals | intRepresentations[i];
+        }
+        // set openAvals to the result
+        openAval = Integer.toString(bitwisedAvals, 2);
+
+        return  openAval;
+    }
 }
